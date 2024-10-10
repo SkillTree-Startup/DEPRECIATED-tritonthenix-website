128086419df176d7a53ce5570f26e9e56d8ccf8a
jak-weston <jak.w.weston@gmail.com>
2024-10-09 19:28:59 -0700

    Weekly Update

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -9,7 +9,7 @@ const Events = () => {
   useEffect(() => {
     const loadEventsData = async () => {
       try {
-        const data = await fetchEventsData();
+        const data = await fetchEventsData(); // Assume this fetches CSV data and includes 'rsvp' field
         setEvents(data);
       } catch (error) {
         console.error('Error fetching events data:', error);
@@ -19,12 +19,17 @@ const Events = () => {
     loadEventsData();
   }, []);
 
-  // Function to generate the Google Form link
-  const generateEventLink = (eventName) => {
+  // Function to generate the Google Form link based on RSVP status
+  const generateEventLink = (eventName, rsvp) => {
     const baseGoogleFormLink = "https://docs.google.com/forms/d/e/1FAIpQLSd58IUJbk6wyxfeBl3k4zwesKz41_f8sbEhwmcGETF44wiO8A/formResponse?entry.1035634596=";
-    return `${baseGoogleFormLink}${encodeURIComponent(eventName)}&entry.928333780=This+event+does+not+require+RSVP&submit=Submit`;
+    const rsvpLink = `https://docs.google.com/forms/d/e/1FAIpQLSd58IUJbk6wyxfeBl3k4zwesKz41_f8sbEhwmcGETF44wiO8A/viewform?usp=sf_link&entry.1035634596=${encodeURIComponent(eventName)}`;
+
+    // If RSVP is required (rsvp = "yes"), return the RSVP link; otherwise, use the generated link
+    return rsvp === 'Yes'
+      ? rsvpLink
+      : `${baseGoogleFormLink}${encodeURIComponent(eventName)}&entry.928333780=This+event+does+not+require+RSVP&submit=Submit`;
   };
-    
+
   return (
     <section className={styles.events}>
       <div className={styles.eventsTop}>
@@ -33,13 +38,13 @@ const Events = () => {
             <Card
               eventName={events[0].eventName}
               description={events[0].description}
-              imageUrl={events[0].imageUrl} // Use updated imageUrl
+              imageUrl={events[0].imageUrl}
               time={events[0].time}
               date={events[0].date}
               location={events[0].location}
               other={events[0].other}
               theme="main event"
-              link={generateEventLink(events[0].eventName)} // Dynamically set the link
+              link={generateEventLink(events[0].eventName, events[0].rsvp)} // Pass 'rsvp' status
               calendar={events[0].calendar}
             />
           </div>
@@ -57,13 +62,13 @@ const Events = () => {
             <Card
               eventName={event.eventName}
               description={event.description}
-              imageUrl={event.imageUrl} // Use updated imageUrl
+              imageUrl={event.imageUrl}
               time={event.time}
               date={event.date}
               location={event.location}
               theme="event"
               other={event.other}
-              link={generateEventLink(event.eventName)} // Dynamically set the link
+              link={generateEventLink(event.eventName, event.rsvp)} // Pass 'rsvp' status
               calendar={event.calendar}
             />
           </div>
